ce4ec3c309966f0665eb880ac0489e38fa3dbe32
luxsanaree <luxsanareem@hotmail.com>
2017-05-29 15:46:05 +0700

    Initial commit

---
+++ src/calculator.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace luxsanaree\caalculator;
+
+class calculator
+{
+    public function add($frist_number, $second_number)
+    {
+ return $frist_number + $second_number;
+    }
+ public function subtract($frist_number, $second_number)
+    {
+ return $frist_number - $second_number;
+    }
+  public function multiply($frist_number, $second_number)
+    {
+ return $frist_number * $second_number;  
+  public function divide($frist_number, $second_number)
+    {
+ return $frist_number / $second_number;
+}
